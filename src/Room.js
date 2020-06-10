f4da36da1c51dc05a0d9afe312e44cb4ad4ed826
mohammaddanial2017 <mohammaddanial2017@gmail.com>
2020-06-10 13:29:48 +0500

    Update basic react state

---
+++ src/Room.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function Room() {
+    // const state = useState(true);
+    // console.log("State = ", state);
+    // const isLit = state[0];
+    // const setLit = state[1];
+
+    const [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(23);
+
+    // function updateLit() {
+    //     console.log("Button Clicked!");
+    //     // isLit = !isLit;
+    //     setLit(!isLit);
+    //     // setAge(++age);
+    // }
+
+    // const increaseAge = () => {
+    //     console.log("Button Clicked!");
+    //     // isLit = !isLit;
+    //     // setLit(!isLit);
+    //     setAge(++age);
+    // }
+
+    return (
+        <div>
+            This Room is {isLit ? 'lit' : 'dark'}
+            <br />
+            Age: {age}
+            <br />
+            {/* <button onClick={updateLit} >Toggle Light</button> */}
+            <button onClick={() => setLit(!isLit)} >Toggle Light</button>
+            <br />
+            <button onClick={() => {
+                console.log("Increase age arrow function!");
+                // isLit = !isLit;
+                // setLit(!isLit);
+                setAge(++age);
+            }} >Increase Age</button>
+        </div>
+    );
+}
+
+export default Room;
